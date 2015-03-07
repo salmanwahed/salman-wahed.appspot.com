7b957c12127f58d872ef52817ddbd2bd1e46b9fb
salman <salman2312@gmail.com>
2015-03-08 00:10:09 +0600

    sendgrid mail send implemented

--- views.py
+++ views.py
@@ -1,18 +1,12 @@
 # -*- coding: UTF-8 -*-
 
-import webapp2
-import jinja2
 import os
 import logging
-from google.appengine.api import mail
-# import sendgrid
+import webapp2
+import jinja2
+import sendgrid
+import settings
 
-
-SMTPserver = 'smtp.sendgrid.net'
-USERNAME = "salmanwahed"
-PASSWORD = "smw@$endGrid"
-destination = ['salman2312@gmail.com']
-text_subtype = 'plain'
 
 logging.getLogger().setLevel(logging.DEBUG)
 file_loader = jinja2.FileSystemLoader(os.path.join(os.path.dirname(__file__), 'blog'))
@@ -24,26 +18,18 @@ env = jinja2.Environment(
 )
 
 
-# def sendgrid_mail(name, email, phone, body):
-#     sg = sendgrid.SendGridClient(USERNAME, PASSWORD)
-#     message = sendgrid.Mail()
-#     message.add_to('Salman Wahed <salman2312@gmail.com>')
-#     message.set_subject("Portfolio- Name: {} Phone: {}".format(name, phone))
-#     message.set_html(body)
-#     message.set_text(body)
-#     message.set_from("{} {}".format(name, email))
-#     status, msg = sg.send(message)
-#     logging.info(status)
-#     return True
-
-
-
-def google_mail(name, email, phone, body):
-    message = mail.EmailMessage(sender="<{}>".format(email),
-                                 subject="Portfolio - Name:{} Phone:{}".format(name, phone))
-    message.to = "Salman Wahed <salman2312@gmail.com>"
-    message.body = body
-    message.send()
+def sendgrid_mail(name, email, phone, body):
+    sg = sendgrid.SendGridClient(settings.USERNAME, settings.PASSWORD)
+    message = sendgrid.Mail()
+    message.add_to(settings.TO_ADDRESS)
+    body = body + "<br /><br /><strong>Name:</strong> {name} <br />" \
+                  "<strong>Phone:</strong> <i>{phone}<i>".format(name=name, phone=phone)
+    message.set_subject("Email from portfolio")
+    message.set_html(body)
+    message.set_text(body)
+    message.set_from("{}".format(email))
+    status, msg = sg.send(message)
+    logging.info(status)
     return True
 
 
@@ -55,20 +41,12 @@ class MyBlog(webapp2.RequestHandler):
 
     def post(self):
         name = self.request.get('name')
-        logging.info("Name: %s" % name)
-
         email = self.request.get('email')
-        logging.info("Email: %s" % email)
-
+        # logging.info("Email: %s" % email)
         phone = self.request.get('phone')
-        logging.info("Phone: %s" % phone)
-
         message = self.request.get('message')
-        logging.info("Message: %s" % message)
 
-        # sendgrid_mail(name, email, phone, message)
-        google_mail(name, email, phone, message)
-
+        sendgrid_mail(name, email, phone, message)
 
 
 application = webapp2.WSGIApplication(
@@ -77,11 +55,3 @@ application = webapp2.WSGIApplication(
     ],
     debug=True
 )
-
-
-# def main():
-# webapp2.util.run_wsgi_app(application)
-#
-#
-# if __name__ == '__main__':
-#     main()
